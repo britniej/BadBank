eec349f3a2809d6a50ebd7fbeda549ca0f3e630e
Britnie Roach <70242995+britniej@users.noreply.github.com>
2022-08-15 23:35:15 -1000

    Add files via upload

--- deposit.js
+++ deposit.js
@@ -1,5 +1,67 @@
+
 function Deposit() {
-    return(
-        <h1>Welcome</h1>
-    );
+  const ctx = React.useContext(UserContext);
+  const user = ctx.users[0].name;
+  const [balance, setBalance] = React.useState("");
+  const [deposit, setDeposit] = React.useState("");
+  const [show, setShow] = React.useState(true);
+
+  function clearForm() {
+    setDeposit("");
+    setShow(true);
+  }
+
+  function handleDeposit() {
+    let deposit = document.getElementById("deposit").value;
+    if (deposit > 0 && !isNaN(deposit)) {
+        user.balance += deposit = balance;
+        setBalance({balance});
+        console.log(balance);
+    } else {
+        alert("Deposit must be a positive number!");
+    }
+  }
+  return (
+    <Card
+      bgcolor="primary"
+      header="Deposit"
+      //status={status}
+      body={
+        show ? (
+          <>
+            <br />
+            Deposit Amount
+            <br />
+            <input
+              type="number"
+              className="form-control"
+              id="deposit"
+              placeholder="0.00"
+              value={deposit}
+              onChange={(e) => setDeposit(e.currentTarget.value)}
+            />
+            <br />
+            <button
+              type="submit"
+              className="btn btn-light"
+              disabled={!deposit}
+              onClick={handleDeposit}
+            >
+              Deposit
+            </button>
+          </>
+        ) : (
+          <>
+            <h3>Successful deposit transaction!</h3>
+            <br />
+            <h5>New balance is user.balance</h5>
+
+            <button type="submit" className="btn btn-light" onClick={clearForm}>
+              Add another account
+            </button>
+          </>
+        )
+      }
+    />
+  );
 }
